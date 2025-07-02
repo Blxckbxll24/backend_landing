a547e3eb4ad6a634ca0d2cf41a4e8dfd4c59be21
Blxckbxll24 <blxckbxll24@gmail.com>
2025-07-02 22:35:31 +0000

    se sube

--- db.js
+++ db.js
@@ -10,5 +10,8 @@ export const pool = mysql.createPool({
   port: process.env.DB_PORT ,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
